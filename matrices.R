1ef0aad1d42315053c773819284ac14ede71cd45
bripapp <brianna.papparotto@gmail.com>
2021-08-24 13:23:18 -0400

    update

--- matrices.R
+++ matrices.R
@@ -1,5 +1,24 @@
 # quickly create sequential numeric vector
-v <- 1:10
+v <- 1:12
 
-# create two dimensional column
+# use vector create two dimensional matrix (10 rows by 1 column)
 matrix(v)
+
+# specify number of rows using nrow
+matrix(v, nrow=2)
+
+# see how byrow argument works
+matrix(v, nrow = 4)
+matrix(v, byrow = FALSE, nrow = 4)
+
+matrix(v, byrow = TRUE, nrow = 4)
+
+# create matrices from vectors
+goog <- c(450,451,452,445,468)
+msft <- c(230,231,232,236,228)
+
+# string one vector after another
+stocks <- c(goog, msft)
+
+# fill in matrix by row, using 2 rows (one for each stock vector)
+stock.matrix <- matrix(stocks, byrow = T, nrow = 2)
