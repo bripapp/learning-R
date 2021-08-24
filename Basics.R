74d8ae619b3e026eca1294bc99ad32935691b3a8
bripapp <brianna.papparotto@gmail.com>
2021-08-24 13:08:53 -0400

    add matrices practice

--- Basics.R
+++ Basics.R
@@ -15,14 +15,20 @@ names(number.list) <- c('Frog', 'Turtle', 'Bear', 'Raccoon', 'Squirrel', 'Chipmu
 mean(stock.prices)
 
 # Create a vector called over.23 consisting of logicals that correspond to the days where the stock price was more than $23
-over.23 <- stock.prices > 23
+stock.prices > 23 # returns a vector of logicals
+
+over.23 <- stock.prices > 23 # assigning vector of logicals (conditional statement) to variable
 
 # Use the over.23 vector to filter out the stock.prices vector and only return the day and prices where the price was over $23
-stock.prices[over.23]
+stock.prices[over.23] # using conditional statement to filter stock.prices
+stock.prices[stock.prices > 23] # same as above 
 
-# Use a built-in function to find the day the price was the highest
-max(stock.prices)
+# Use a built-in function to find the DAY the price was the highest
+max(stock.prices) # only returns highest stock price, not the day
 
-max.price <- stock.prices == max(stock.prices)
+# search through vector and names to see where stock.prices equales the max stock price
+max.price <- stock.prices == max(stock.prices) 
+
+stock.prices[max.price]
 
 
---
+++ matrices.R
@@ -0,0 +1,5 @@
+# quickly create sequential numeric vector
+v <- 1:10
+
+# create two dimensional column
+matrix(v)
