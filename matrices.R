a4c58c64fdaf7ad4c76d084bd04ea69309a9f69c
bripapp <brianna.papparotto@gmail.com>
2021-08-29 14:35:52 -0400

    matrix indexing

--- matrices.R
+++ matrices.R
@@ -65,3 +65,54 @@ mat*mat # this is NOT the same as matrix multiplication
 
 # true matrix multiplication in linear algebra (not element by element)
 mat %*% mat
+
+######
+# MATRIX OPERATIONS 
+######
+
+# sum up values in each column
+colsSums(stock.matrix)
+# sum up values in each row
+rowSums(stock.matrix)
+# get the mean value of each row
+rowMeans(stock.matrix)
+
+FB <- c(111,112,113,120,145)
+# use rbind() to create a new row
+tech.stocks <- rbind(stock.matrix,FB)
+
+# create avg vector and use cbind() to create a new column
+avg <- rowMeans(tech.stocks)
+tech.stocks <- cbind(tech.stocks,avg)
+
+######
+# MATRIX SELECTION AND INDEXING
+# syntax:
+# example.matrix[rows,columns]
+######
+
+
+# create a matrix with 50 elements, fill in by row, split into 5 rows
+mat <- matrix(1:50, byrow=TRUE, nrow=5)
+
+# grab first row
+mat[1,]
+
+# grab first column
+mat[,1] # notice comma placement
+
+# grab first 3 rows
+mat[1:3,]
+
+# grab top left rectangle of:
+# 1,2,3
+# 11,12,13
+mat[1:2, 1:3]
+
+# grab last two columns
+mat[,9:10]
+
+# grab a center square of:
+# 15,16
+# 25,26
+mat[2:3,5:6]
